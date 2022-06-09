0402591c1faafe7b61f3e8c05feab82e48256dad
allan-gilber <allangilber@yahoo.com.br>
2022-06-09 20:11:17 -0300

    feat: added a simple css file for home.

--- src/presentation/Home/index.tsx
+++ src/presentation/Home/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import {MainContainer} from './style';
 
 export default function Home(): any{
   return (
-    <div>
+    <MainContainer>
       <p>Hello</p>
       <h1>{`teste env: ${process.env.REACT_APP_TEST}`}</h1>
-    </div>
+    </MainContainer>
   );
 }
